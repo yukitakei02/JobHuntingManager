e08c9db24d8bffb7c9e8c8d1969e0fca900874d6
yukitakei02 <take28.07.02@gmail.com>
2026-04-17 19:10:20 +0900

    Initial Commit

---
+++ JobHuntingManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobHuntingManager
+//
+//  Created by 竹井佑騎 on 2026/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobHuntingManager/JobHuntingManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobHuntingManagerApp.swift
+//  JobHuntingManager
+//
+//  Created by 竹井佑騎 on 2026/04/17.
+//
+
+import SwiftUI
+
+@main
+struct JobHuntingManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
